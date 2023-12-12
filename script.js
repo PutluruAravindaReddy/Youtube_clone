be1172ec5e004423096293fa1e22cee10a757907
PutluruAravindaReddy <aravindareddy60@gmail.com>
2023-12-12 12:42:44 +0530

    Restructed css

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-var featuresclick=document.getElementsByClassName("features");
-var sbclick=document.getElementsByClassName("sb-box");
 
-for(let i=0;i<featuresclick.length;i++){
-    featuresclick[i].addEventListener("click",function(){
-        var activef=document.getElementsByClassName("activef");
+var featuresclick = document.getElementsByClassName("features");
+var sbclick = document.getElementsByClassName("sb-box");
+var showmore = document.querySelector(".show-more");
+var showless = document.querySelector(".show-less");
+var remchannels = document.getElementsByClassName("extra-channels");
+
+
+
+for (let i = 0; i < featuresclick.length; i++) {
+    featuresclick[i].addEventListener("click", function () {
+        var activef = document.getElementsByClassName("activef");
         for (let j = 0; j < activef.length; j++) {
             activef[j].classList.remove("activef");
         }
@@ -11,15 +17,33 @@ for(let i=0;i<featuresclick.length;i++){
     });
 }
 
-for(let i=0;i<sbclick.length;i++){
-    sbclick[i].addEventListener("click",function(){
-        var activesb=document.getElementsByClassName("activesb");
+for (let i = 0; i < sbclick.length; i++) {
+    sbclick[i].addEventListener("click", function () {
+        var activesb = document.getElementsByClassName("activesb");
         for (let j = 0; j < activesb.length; j++) {
             activesb[j].classList.remove("activesb");
         }
         this.classList.add("activesb");
     });
 }
+
+showmore.addEventListener("click", function () {
+        for (let i = 0; i < remchannels.length; i++) {
+            remchannels[i].classList.remove("channels-hide");
+        }
+        showmore.classList.add("channels-hide");
+});
+
+showless.addEventListener("click", function () {
+    for (let i = 0; i < remchannels.length; i++) {
+        remchannels[i].classList.add("channels-hide");
+    }
+    showmore.classList.remove("channels-hide");
+    showless.classList.add("channels-hide");
+});
+
+
+
 // window.addEventListener('scroll', function() {
 //     // Get the features section and videos section
 //     var featuresSection = document.getElementById('features-sec');
