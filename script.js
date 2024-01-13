7b87fde1767ec6f9c2654574e481e5c5110ce7a2
PutluruAravindaReddy <aravindareddy60@gmail.com>
2024-01-13 17:07:10 +0530

    Update

--- script.js
+++ script.js
@@ -179,11 +179,16 @@ showless.addEventListener("click", function () {
 });
 
 function toggleSidebar() {
-    var sidebar = document.getElementById('sidebar');
-    if (sidebar.style.display === 'none' || sidebar.style.display === '') {
-        sidebar.style.display = 'block';
-    } else {
-        sidebar.style.display = 'none';
+    var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+    // Only toggle the sidebar if the screen size is less than 440px
+    if (screenWidth < 440) {
+        var sidebar = document.getElementById('sidebar');
+        if (sidebar.style.display === 'none' || sidebar.style.display === '') {
+            sidebar.style.display = 'block';
+        } else {
+            sidebar.style.display = 'none';
+        }
     }
 }
 
